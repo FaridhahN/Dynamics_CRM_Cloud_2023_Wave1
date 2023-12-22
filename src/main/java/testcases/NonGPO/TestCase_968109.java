345d726b566ec4913bf47a8e4b6b6d112bc15ecc
vkamala2 <vkamala2@Z3P5PUGDEV0059.corp.premierinc.com>
2023-12-22 09:31:28 +0530

    Committing Non GPO Test cases

---
+++ src/main/java/testcases/NonGPO/TestCase_968109.java
@@ -0,0 +1,253 @@
+package testcases.NonGPO;
+
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+import utils.DataInputProvider;
+//TFS ID_968109:_Verify NONGPO membership is end dated when account has both GPO and NONGPO memberships
+
+
+public class TestCase_968109 {
+
+	@Test
+	public void endGPOMEmbership(int iRowNumber, String sDataSheetName) throws Exception, InterruptedException  {
+
+		//1. Login to CRM using member supervisor / member credentials 
+		new LoginPage()
+
+		.typeEmail(DataInputProvider.getCellData_ColName(iRowNumber, "email", sDataSheetName))
+		.clickNext()
+		.typePassword(DataInputProvider.getCellData_ColName(iRowNumber, "password", sDataSheetName))  
+		.clicSignin()
+
+		.clicYesInStaySignedin()
+
+		//2. From the left navigation column ,Go to Accounts > +New
+		.selectAccountsTab()
+
+		.clickNewOnAccountsPage()
+		.chooseMemberForm()
+
+		//3. Account Name = Any
+		.typeAccountName(DataInputProvider.getCellData_ColName(iRowNumber, "accountName", sDataSheetName)+1)
+
+		//Click on save 			
+		.clickSave() 
+
+		//4. Verify CRM Account # is generated 
+		.verifyCRMNumberIsDisplayed()	
+
+		//5. Account Type = Member
+		.selectAccountType(DataInputProvider.getCellData_ColName(iRowNumber, "accountType", sDataSheetName))
+
+		//Class of Trade =Any
+		.selectClassOfTrade(DataInputProvider.getCellData_ColName(iRowNumber, "classOfTrade", sDataSheetName))
+
+		//Business Classification = Auto populated
+		.verifyBusinessClassification(DataInputProvider.getCellData_ColName(iRowNumber, "verifyBusinessClassification", sDataSheetName))
+
+		//Account Status = Auto Populated to Active
+		.verifyDefaultAccountStatus()	
+
+		//Application Start Date = Today's Date
+		.chooseApplicationDate(DataInputProvider.getCellData_ColName(iRowNumber, "applicationDate", sDataSheetName))
+
+		//CAMS Flag = Yes
+		.changeCAMSFlagAsYes()
+
+
+		//Participation Type = Standard
+		.selectParticipationType("Cellular Services Only")
+
+
+		//Direct Parent Entity Code = 673415
+		.selectDirectParent(DataInputProvider.getCellData_ColName(iRowNumber, "directParent", sDataSheetName))
+
+		//Direct Parent Relation = Managed
+		.selectDirectParentRelationManaged() 
+
+		//Direct Parent Relation date = Today's Date
+		.selectDirectParentRelationDate(DataInputProvider.getCellData_ColName(iRowNumber, "directParentRelationDate", sDataSheetName))
+
+		//Top Parent Relation =  OLM
+		.selectTopParentRelation(DataInputProvider.getCellData_ColName(iRowNumber, "topParentRelation", sDataSheetName))
+
+		// Top Parent Relation Date = Today's Date
+		.selectTopParentRelationDate( DataInputProvider.getCellData_ColName(iRowNumber, "topParentRelationDate", sDataSheetName))
+
+		//Click on Save 
+		.clickSave() 
+
+		//6. Street 1 = Any
+		.typeStreet1(DataInputProvider.getCellData_ColName(iRowNumber, "street1", sDataSheetName))
+
+		//City = NY
+		.typeCity(DataInputProvider.getCellData_ColName(iRowNumber, "city", sDataSheetName))
+
+		//Country =USA
+		.typeCountry(DataInputProvider.getCellData_ColName(iRowNumber, "country", sDataSheetName))
+
+		//Type Zip code
+		.typeZipCode(DataInputProvider.getCellData_ColName(iRowNumber, "ZipCode", sDataSheetName))
+
+		//Click on Save 
+		.clickSave() 
+
+		//Click add new membership
+		.clickMembershipAndAddNewMembership()
+
+		// Choose Membership type 
+		.selectMembershipType(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProviderType", sDataSheetName))
+		.selectMembershipProvider(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProvider", sDataSheetName))
+
+		//Provide any start date and click on save
+		.typeMembershipStartDate(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProviderStartDate", sDataSheetName))
+
+		//Click on membership save and close
+		.clickQuickCreateMembershipSaveAndClose()
+
+		.clickMembershipAndAddNewMembership()
+
+		// Choose Membership type 
+		.selectMembershipType(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProviderType", sDataSheetName))
+		.selectMembershipProvider(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProvider2", sDataSheetName))
+
+		//Provide any start date and click on save
+		.typeMembershipStartDate(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProviderStartDate2", sDataSheetName))
+
+		//Click on membership save and close
+		.clickQuickCreateMembershipSaveAndClose()
+
+
+		//6.Click the + icon on the Line of Business Grid ***** Corresponding LOB should be added successfully 
+		.clickLineOfBusinesses()
+
+		//Click New Line Of Business
+		.clickAddNewLineOfBusiness()
+
+		// Line of Business =General GPO
+		.selectLineOfBusiness(DataInputProvider.getCellData_ColName(iRowNumber, "lineOfBusiness", sDataSheetName))
+
+		// Classification Type = General GPO
+		.selectLOBfClassificationType(DataInputProvider.getCellData_ColName(iRowNumber, "lineOfClassification", sDataSheetName))
+
+		// Start Date =Today's date
+		.typeLineOfBusinessStartDate(DataInputProvider.getCellData_ColName(iRowNumber, "lineOfBusinessStartDate", sDataSheetName))
+
+		// Click on LOB Save 
+		.clickLOBSaveAndClose()
+
+
+		.clickGeneralTab()
+
+		//8. Record Status = Published
+		.chooseRecordStatusPublished()
+
+		//Click on Save 
+		.clickSave() 
+
+		//9. Verify Entity code is generated 
+		.entityCodeIsDisplayed()
+
+		//9.Move the record status to draft and save  ***** Record moved to draft 
+		.chooseRecordStatusDraft()
+
+		//Click on Save 
+		.clickSave() 
+
+		//10.Go to membership and Open the Premier National membership ***** Premier National membership should be opened 
+		.goToMembershipPage(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProvider", sDataSheetName))
+		//.doubleClickOnNationalMembership()
+
+		//11.Provide end date = Any future date **** Account should be saved successfully 
+		.typeMembershipEndDate(DataInputProvider.getCellData_ColName(iRowNumber, "membershipEndDate", sDataSheetName))
+
+		// End reason = Anything from dropdown,
+		.selectMembershipEndReason(DataInputProvider.getCellData_ColName(iRowNumber, "membershipEndReason", sDataSheetName))
+
+		// then save
+		.clickMembershipSaveAndClose()
+
+		//12.Verify the account status  ***** Account status should become terminated 
+		.pageRefresh()
+		.verifyAccountStatus(DataInputProvider.getCellData_ColName(iRowNumber, "accountStatus", sDataSheetName))
+
+		//13.Verify Premier end date **** Premier end date should be populated with the same date  as National membership end date 
+		.verifyPremierEndDate(DataInputProvider.getCellData_ColName(iRowNumber, "membershipEndDate", sDataSheetName))
+
+		//15. Now move the record status to Published and save **** Account should be published successfully 
+		.chooseRecordStatusPublished()	
+
+		//Click on Save 
+		.clickSave() 
+
+		//16.For reactivation, move the record status to draft *** Record should be moved to draft 
+		.chooseRecordStatusDraft()
+
+		//Click on Save 
+		.clickSave()
+
+		//17.Go to membership and add New membership **** Account should be saved successfully 
+		//Add Membership provider
+		//Click add new membership
+		.clickMembershipAndAddNewMembership()
+
+		// Choose Membership type 
+		.selectMembershipType(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProviderType", sDataSheetName))
+		.selectMembershipProvider(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProvider2", sDataSheetName))
+
+		//Provide any start date and click on save
+		.typeMembershipStartDate(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProviderStartDate2", sDataSheetName))
+
+		//Click on membership save and close
+		.clickQuickCreateMembershipSaveAndClose()	
+
+		//18.Now add corresponding LOB **** LOB should be added successfully 
+		.clickLineOfBusinesses()
+
+		//Click New Line Of Business
+		.clickAddNewLineOfBusiness()
+
+		// Line of Business =General GPO
+		.selectLineOfBusiness(DataInputProvider.getCellData_ColName(iRowNumber, "lineOfBusiness2", sDataSheetName))
+
+		// Classification Type = General GPO
+		.selectLOBfClassificationType(DataInputProvider.getCellData_ColName(iRowNumber, "lineOfClassification2", sDataSheetName))
+
+		// Start Date =Today's date
+		.typeLineOfBusinessStartDate(DataInputProvider.getCellData_ColName(iRowNumber, "lineOfBusinessStartDate2", sDataSheetName))
+
+		// Click on LOB Save 
+		.clickLOBSaveAndClose()
+
+
+		//25.Move the record status to Published ***** Account should be published and reactivated successfully 
+		.chooseRecordStatusPublished()
+
+		//Click on Save 
+		.clickSave() 
+
+
+		//10.Go to membership and Open the Premier National membership ***** Premier Non GPO membership should be opened 
+		.goToMembershipPage(DataInputProvider.getCellData_ColName(iRowNumber, "membershipProvider", sDataSheetName))
+		//.doubleClickOnNationalMembership()
+
+		//11.Provide end date = Any future date **** Account should be saved successfully 
+		.typeMembershipEndDate(DataInputProvider.getCellData_ColName(iRowNumber, "membershipEndDate", sDataSheetName))
+
+		// End reason = Anything from dropdown,
+		.selectMembershipEndReason(DataInputProvider.getCellData_ColName(iRowNumber, "membershipEndReason", sDataSheetName))
+
+		// then save
+		.clickMembershipSaveAndClose()
+
+		//12.Verify the account status  ***** Account status should become terminated 
+		.pageRefresh()
+		.verifyAccountStatus(DataInputProvider.getCellData_ColName(iRowNumber, "accountStatus1", sDataSheetName))
+
+
+
+
+		;
+	}
+}
