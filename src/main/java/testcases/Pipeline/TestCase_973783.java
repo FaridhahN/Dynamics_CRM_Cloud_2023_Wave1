5b7f4ccd214786d2337776593ce729c87a80f89b
vkamala2 <vkamala2@Z3P5PUGDEV0059.corp.premierinc.com>
2024-02-29 16:27:20 +0530

    Pipeline Test cases

---
+++ src/main/java/testcases/Pipeline/TestCase_973783.java
@@ -0,0 +1,36 @@
+package testcases.Pipeline;
+
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import utils.DataInputProvider;
+
+//TFS ID_973783:_973783:_Verify existing stage 3 records are updated as new Stage 3 value "Proposal/Negotiations"
+
+public class TestCase_973783 {
+
+	@Test
+	public void verifyPipelineStage(int iRowNumber, String sDataSheetName) throws Exception, InterruptedException  {
+
+		//1. Login to CRM as Member with Pipeline Steward Role
+		new LoginPage()
+		.typeEmail(DataInputProvider.getCellData_ColName(iRowNumber, "email", sDataSheetName))
+		.clickNext()
+		.typePassword(DataInputProvider.getCellData_ColName(iRowNumber, "password", sDataSheetName))  
+		.clicSignin()
+		.clicYesInStaySignedin()
+
+		//2. Go to Workplace >> Take Any Member Account 
+		.selectAccountsTab()		
+		.searchAccount(DataInputProvider.getCellData_ColName(iRowNumber, "crmNumber", sDataSheetName)) 
+		.selectAccountFromGlobalSearchResults(DataInputProvider.getCellData_ColName(iRowNumber, "CrmNumber", sDataSheetName))
+
+		.navigateToPiepline()
+		.clickNewPipeline()
+		.verifyPipelinePage()
+		.verifyPipelineStageOption(DataInputProvider.getCellData_ColName(iRowNumber, "pipelineStage", sDataSheetName))
+		;
+
+
+
+	}
+}
